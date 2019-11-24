1e7d7104804ef33914f2c3cc694ea430f7c01a09
taochenshh <chentao904@163.com>
2019-11-24 11:17:30 -0500

    add tanh as activation

---
+++ examples/ppo_tanh.py
@@ -0,0 +1,45 @@
+import torch.nn as nn
+
+from easyrl.agents.ppo import PPOAgent
+from easyrl.configs.command_line import cfg_from_cmd
+from easyrl.configs.ppo_config import ppo_cfg
+from easyrl.engine.ppo_engine import PPOEngine
+from easyrl.models.diag_gaussian_policy import DiagGaussianPolicy
+from easyrl.models.mlp import MLP
+from easyrl.models.value_net import ValueNet
+from easyrl.runner.episodic_runner import EpisodicRunner
+from easyrl.utils.common import set_random_seed
+from easyrl.utils.gym_util import make_vec_env
+
+
+def main():
+    cfg_from_cmd(ppo_cfg)
+    if ppo_cfg.resume:
+        ppo_cfg.restore_cfg()
+    set_random_seed(ppo_cfg.seed)
+    env = make_vec_env(ppo_cfg.env_id, ppo_cfg.num_envs)
+    env.reset()
+    ob_size = env.observation_space.shape[0]
+    act_size = env.action_space.shape[0]
+    actor_body = MLP(input_size=ob_size,
+                     hidden_sizes=[64],
+                     output_size=64,
+                     hidden_act=nn.Tanh,
+                     output_act=nn.Tanh)
+    critic_body = MLP(input_size=ob_size,
+                      hidden_sizes=[64],
+                      output_size=64,
+                      hidden_act=nn.Tanh,
+                      output_act=nn.Tanh)
+    actor = DiagGaussianPolicy(actor_body, action_dim=act_size)
+    critic = ValueNet(critic_body)
+    agent = PPOAgent(actor, critic)
+    runner = EpisodicRunner(agent=agent, env=env)
+    engine = PPOEngine(agent=agent,
+                       env=env,
+                       runner=runner)
+    engine.train()
+
+
+if __name__ == '__main__':
+    main()
--- examples/run.sh
+++ examples/run.sh
@@ -7,9 +7,13 @@ trap "kill 0" EXIT
 #python ppo.py --env_id='Walker2d-v2' --seed=234 &
 #python ppo.py --env_id='Ant-v2' --seed=234 &
 #wait
-KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='Swimmer-v2' &
-KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='Swimmer-v2' --seed=234 &
-KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='HalfCheetah-v2' &
-KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='HalfCheetah-v2' --seed=234 &
+#KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='Swimmer-v2' &
+#KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='Swimmer-v2' --seed=234 &
+#KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='HalfCheetah-v2' &
+#KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='HalfCheetah-v2' --seed=234 &
+KMP_INIT_AT_FORK=FALSE python ppo_tanh.py --env_id='Swimmer-v2' --save_dir='tanh_data' &
+KMP_INIT_AT_FORK=FALSE python ppo_tanh.py --env_id='Swimmer-v2' --seed=234 --save_dir='tanh_data' &
+KMP_INIT_AT_FORK=FALSE python ppo_tanh.py --env_id='HalfCheetah-v2' --save_dir='tanh_data' &
+KMP_INIT_AT_FORK=FALSE python ppo_tanh.py --env_id='HalfCheetah-v2' --seed=234 --save_dir='tanh_data' &
 #python ppo.py --env_id='Ant-v2' &
 wait
