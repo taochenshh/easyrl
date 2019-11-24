d885a06c282a0a5a2febdd275a3c2ab3e656c842
taochenshh <chentao904@163.com>
2019-11-24 00:07:32 -0500

    modify run file

--- examples/run.sh
+++ examples/run.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+trap "exit" INT TERM ERR
+trap "kill 0" EXIT
 python ppo.py --env_id='Hopper-v2' &
 python ppo.py --env_id='Hopper-v2' --seed=234 &
 python ppo.py --env_id='Walker2d-v2' &
@@ -9,3 +12,4 @@ python ppo.py --env_id='Swimmer-v2' --seed=234 &
 python ppo.py --env_id='HalfCheetah-v2' &
 python ppo.py --env_id='HalfCheetah-v2' --seed=234 &
 python ppo.py --env_id='Ant-v2' &
+wait
