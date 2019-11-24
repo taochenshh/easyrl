83ba7e7cdd67cfc502edc1660c33603f4a63393e
taochenshh <chentao904@163.com>
2019-11-24 13:07:08 -0500

    minor bug

--- easyrl/utils/torch_util.py
+++ easyrl/utils/torch_util.py
@@ -1,6 +1,7 @@
 import math
 
 import torch
+import torch.nn as nn
 from torch.distributions import Categorical
 from torch.distributions import Independent
 from torch.distributions import Transform
