d6e5bcfa118db683246267a30d47afb6414151a4
taochenshh <chentao904@163.com>
2019-11-24 11:02:15 -0500

    update run.sh

--- examples/run.sh
+++ examples/run.sh
@@ -7,9 +7,9 @@ trap "kill 0" EXIT
 #python ppo.py --env_id='Walker2d-v2' --seed=234 &
 #python ppo.py --env_id='Ant-v2' --seed=234 &
 #wait
-python ppo.py --env_id='Swimmer-v2' &
-python ppo.py --env_id='Swimmer-v2' --seed=234 &
-python ppo.py --env_id='HalfCheetah-v2' &
-python ppo.py --env_id='HalfCheetah-v2' --seed=234 &
+KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='Swimmer-v2' &
+KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='Swimmer-v2' --seed=234 &
+KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='HalfCheetah-v2' &
+KMP_INIT_AT_FORK=FALSE python ppo.py --env_id='HalfCheetah-v2' --seed=234 &
 #python ppo.py --env_id='Ant-v2' &
 wait
